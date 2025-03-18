8eee7a4e59dc70ee32ecb5f5e6156967a0f8be6f
dinoagera <120749826+dinoagera@users.noreply.github.com>
2025-03-18 23:14:47 +0300

    Delete migrations directory

--- migrations/20250312175007_urlshortener.up.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE urlshortener (
-    id bigserial NOT NULL PRIMARY KEY,
-    url TEXT NOT NULL,
-    name VARCHAR NOT NULL UNIQUE
-);
-CREATE INDEX idx_urlshortener_name ON urlshortener (name);
